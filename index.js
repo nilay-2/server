19e066545ba125875bdfedec4b4af76c1cb4652c
Nilay Pophalkar <nilaypophalkar@gmail.com>
2022-12-22 19:19:21 +0530

    trying to deploy this app

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.use("/", clientDetailRouter);
 app.use(globalErrHandler);
 const port = process.env.PORT || 5000;
 app.listen(port, "127.0.0.1", () => {
-  console.log(`App running on port ${process.env.PORT}`);
+  console.log(`App running on port ${port}`);
 });
