30ed2ab4e65650b90020dd4e9dbe5851c04d42ff
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-01-06 13:43:48 +0530

    added file upload

--- controller/clientDetailController.js
+++ controller/clientDetailController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const multer = require("multer");
+const sharp = require("sharp");
 exports.getClientDetails = async (req, res, next) => {
   try {
     res.status(200).json({
@@ -39,8 +40,19 @@ const upload = multer({
 
 exports.fileParser = upload.single("file");
 
-exports.fileUploader = (req, res, next) => {
+exports.resizeUserPhoto = async (req, res, next) => {
   console.log(req.file);
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+  next();
+};
+
+exports.fileUploader = (req, res, next) => {
   res.status(200).json({
     status: "success",
     message: "file uploaded successfully",
--- routes/ClientDetailRouter.js
+++ routes/ClientDetailRouter.js
@@ -14,6 +14,7 @@ router
   .post(
     userController.protect,
     clientDetailController.fileParser,
+    clientDetailController.resizeUserPhoto,
     clientDetailController.fileUploader
   );
 
