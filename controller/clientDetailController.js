21c81026ea5cf416048f8472b5c8771b79d0dfff
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-05-20 20:30:26 +0530

    added image resizer

--- controller/clientDetailController.js
+++ controller/clientDetailController.js
@@ -40,19 +40,35 @@ const upload = multer({
 
 exports.fileParser = upload.single("file");
 
-exports.resizeUserPhoto = (req, res, next) => {
+// exports.resizeUserPhoto = (req, res, next) => {
+//   if (!req.file) return next();
+//   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+//   sharp(req.file.buffer)
+//     .resize(500, 500)
+//     .toFormat("jpeg")
+//     .jpeg({ quality: 90 })
+//     .toBuffer()
+//     .then((data) => {
+//       const base64data = data.toString("base64");
+//       res.status(200).json({ data: base64data, contentType: "image/jpeg" });
+//     });
+//   // .toFile(`public/img/users/${req.file.filename}`);
+// };
+
+exports.resizeUserPhoto = async (req, res, next) => {
   if (!req.file) return next();
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  sharp(req.file.buffer)
+  const resizedImage = await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat("jpeg")
     .jpeg({ quality: 90 })
-    .toBuffer()
-    .then((data) => {
-      const base64data = data.toString("base64");
-      res.status(200).json({ data: base64data, contentType: "image/jpeg" });
-    });
+    .toBuffer();
+  // .then((data) => {
+  //   const base64data = data.toString("base64");
+  // });
+  const base64data = resizedImage.toString("base64");
   // .toFile(`public/img/users/${req.file.filename}`);
+  res.status(200).json({ data: base64data, contentType: "image/jpeg" });
 };
 
 // exports.fileUploader = (req, res, next) => {
